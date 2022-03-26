6ee6e91934ffb19b9214f2a1b89812a6892196c8
imhalid <imhalid@icloud.com>
2022-03-26 12:30:54 +0300

    first commit

---
+++ web/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="timeline">
+      <div id="content"></div>
+      <div class="card">
+        <div class="icon"></div>
+        <div class="content">
+          <b>ハリドイスラム</b>
+          <p>自己学習型フロントエンド開発者</p>
+        </div>
+        <div class="arrow"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
