a2f2d34e22e592bcada6c81e67fef9bc11576783
imhalid <imhalid@icloud.com>
2023-12-23 13:34:34 +0300

    Update canvas and scene components

--- app/components/canvas/scene.jsx
+++ app/components/canvas/scene.jsx
@@ -7,16 +7,16 @@ import { Perf } from "r3f-perf";
 
 export default function Scene() {
   return (
-    <group position={[0, 0, 0]}>
+    <group position={[5, 0, 0]}>
       <Perf />
-      {/* <Lights /> */}
-      <Spheres count={45} radius={3.5} speed={2} />
-      <Spheres count={40} radius={3} speed={2.1} />
-      <Spheres count={35} radius={2.5} speed={2.2} />
-      <Spheres count={30} radius={2} speed={2.3} />
-      <Spheres count={25} radius={1.5} speed={2.4} />
-      <Spheres count={20} radius={1} speed={2.5} />
-      <Spheres count={15} radius={0.5} speed={2.6} />
+      <Lights />
+      <mesh rotation={[1,1,1]}>
+        <boxGeometry args={[1,1,1]} />
+        <meshStandardMaterial color={0x80ffdb} />
+      </mesh>
+      {Array.from({ length: 5 }, (_, index) => (
+        <Spheres count={50 * (index / 2)} radius={1 * index} speed={1 * index} key={index} index={index} />
+      ))}
     </group>
   )
 }
@@ -31,7 +31,7 @@ function Lights() {
   );
 }
 
-function Spheres({ count = 50, radius = 4, centerX = 0, centerY = 0, centerZ = 0, color = new THREE.Color(), speed = 1, ...props }) {
+function Spheres({ count = 50, radius = 4, centerX = 0, centerY = 0, centerZ = 0, color = new THREE.Color(), speed = 1, index, ...props }) {
   const ref = useRef()
   const shaderMaterial = useRef()
   // Daire merkezi
@@ -45,32 +45,30 @@ function Spheres({ count = 50, radius = 4, centerX = 0, centerY = 0, centerZ = 0
     const x = centerX + radius * Math.cos(angle);
     const y = centerY + radius * Math.sin(angle);
     const z = 0; // Z koordinatını sabitleyin
-
     return {
       position: [x, y, z],
-      color: new THREE.Color("orange")
+      // color: new THREE.Color(0x909090)
     };
   });
+  
+  const rainbowColors = [0x7400b8, 0x6930c3, 0x5e60ce, 0x5390d9, 0x4ea8de, 0x48bfe3, 0x56cfe1, 0x64dfdf, 0x72efdd, 0x80ffdb]
 
+console.log(index % rainbowColors.length)
   useFrame((state, delta) => {
     const time = state.clock.getElapsedTime()
-    shaderMaterial.current.uTime = time
+    // shaderMaterial.current.uTime = time
     ref.current.rotation.set(
       0,
-      // 2 * Math.PI * (time / speed * 0.05),
-      0,
-      0
-
+      ref.current.rotation.y = Math.PI * 2 * (time / speed * .2) % (Math.PI * 2),
+      // 0,
+      ref.current.rotation.y = Math.PI * 2 * (time / speed * .2) % (Math.PI * 2)
     )
   })
-
-  const material = <sphereShaderMaterial ref={shaderMaterial} />
-  const geometry = <sphereGeometry args={[0.05, 20, 20]} />
   return (
-    <Instances ref={ref}>
-      <sphereGeometry args={[0.05, 20, 20]} />
-      {/* <meshStandardMaterial roughness={1} /> */}
-      <sphereShaderMaterial ref={shaderMaterial} />
+    <Instances ref={ref} >
+      <boxGeometry args={[0.1, 0.1, 0.1]} />
+      <meshStandardMaterial color={rainbowColors[index % rainbowColors.length]} />
+      {/* <sphereShaderMaterial ref={shaderMaterial} /> */}
       {spheresValue.map((props, index) => (
         <Sphere key={index} {...props} />
       ))}
@@ -87,6 +85,12 @@ function Sphere({ color = new THREE.Color(), position, ...props }) {
       position[1],
       position[2]
     )
+    ref.current.rotation.set(
+      0,
+      // Math.sin(2 * Math.PI * (time / 2 * 0.2)),
+      0,
+      0
+    )
 
   })
 
@@ -96,19 +100,26 @@ function Sphere({ color = new THREE.Color(), position, ...props }) {
 }
 
 const SphereShaderMaterial = shaderMaterial({
-  uTime: 0,
+  uTime: 1, uRotaion: 1.5, uFrequency:0.5, uAmplitude:0.5,
 },
   `
   varying vec3 vNormal;
   varying vec2 vUv;
   uniform float uTime;
+  uniform float uRotation;
+  uniform float uFrequency;
+  uniform float uAmplitude;
+  attribute vec3 aPos;
 
   void main() {
     vec3 pos = position;
     vNormal = normal;
     vUv = uv;
 
-    gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(pos, 1.0);
+    vec4 modelPosition = instanceMatrix * vec4(aPos, 1.0);
+
+    
+    gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(pos, 1.);
   }
   `,
   `
@@ -116,12 +127,11 @@ const SphereShaderMaterial = shaderMaterial({
   varying vec2 vUv;
   uniform float uTime;
   void main() {
-    vec3 color = vec3(1.0, 0.0, 0.0);
-    float light = dot(normalize(vNormal), normalize(vec3(0.0, 0.0, 1.0)));
-    gl_FragColor = vec4(vUv , 0.0, 1.0);
+
+    vec2 neWuv = -1.0 + 2.0 * vUv;
+    gl_FragColor = vec4(0., -neWuv, 1.);
   }
   `
-
 )
 
 extend({ SphereShaderMaterial })
