5e88395628c35e7d779165d93b956d78801be66d
imhalid <imhalid@icloud.com>
2023-12-28 13:56:27 +0300

    Update project image URLs to use remote GitHub URLs

--- components/data/projects.js
+++ components/data/projects.js
@@ -3,7 +3,7 @@ const projects = [
     id: 9,
     name: 'Hikarix',
     description: 'A wonderfully designed movie search platform with R3F (React Three Fiber).',
-    image: '/projects/hikarix.jpg',
+    image: 'https://github.com/imhalid/website/assets/40598819/0857634e-1908-4fec-97b9-e3803ee2be98',
     link: 'https://github.com/imhalid/hikarix',
     live: 'https://hikarix.vercel.app',
     tags: ['R3F', 'Next.js', 'CSS', 'API', 'TypeScript'],
@@ -13,7 +13,7 @@ const projects = [
     name: 'CV Builder',
     description:
       'Beautifully designed cv builder where you can see the changes at the same time',
-    image: '/projects/cvbuilder.png',
+    image: 'https://github.com/imhalid/website/assets/40598819/9418fdba-bd28-49a6-8a8c-9ad948835a47',
     link: 'https://github.com/imhalid/cv-builder',
     live: 'https://cv-builder.halid.dev',
     tags: ['TailwindCSS', 'JSON', 'Next.js'],
@@ -23,7 +23,7 @@ const projects = [
     name: 'halid.dev',
     description:
       'My personal website, that I created with Next.js, Tailwind Css, Content Layer',
-    image: '/projects/halid.png',
+    image: 'https://github.com/imhalid/website/assets/40598819/2dfb3dbb-bc83-437f-85df-989447336e44',
     link: 'https://github.com/imhalid/halid.dev',
     live: '/',
     tags: ['TailwindCSS', 'MDX', 'Next.js'],
@@ -32,7 +32,7 @@ const projects = [
     id: 4,
     name: 'Tip Calculator',
     description: 'An application that will facilitate your tip calculations',
-    image: '/projects/tip.png',
+    image: 'https://github.com/imhalid/website/assets/40598819/983eede1-4ef6-4b6f-9f0f-3aece2cb579a',
     link: 'https://github.com/imhalid/tip-calculator',
     live: 'http://tip-calculator-eta-six.vercel.app/',
     tags: ['Javascript', 'CSS'],
@@ -42,7 +42,7 @@ const projects = [
     name: 'Advice Generator',
     description:
       'An application that displays data from the API on the screen.',
-    image: '/projects/advice.png',
+    image: 'https://github.com/imhalid/website/assets/40598819/b7f467fd-e34d-4cb7-8210-271b7a8646d9',
     link: 'https://github.com/imhalid/advice-generator',
     live: 'http://advice-generator-imhalid.vercel.app/',
     tags: ['Javascript', 'CSS', 'API'],
@@ -52,7 +52,7 @@ const projects = [
     name: 'Expenses Chart Component',
     description:
       'The project I developed with React.js converts the data from json into graphics.',
-    image: '/projects/chart.png',
+    image: 'https://github.com/imhalid/website/assets/40598819/78c9c0a6-844d-4631-a5ff-a82babf24696',
     link: 'https://github.com/imhalid/expenses-chart-component-main',
     live: 'http://expenses-chart-component-main-henna.vercel.app/',
     tags: ['React.js', 'Json', 'CSS'],
