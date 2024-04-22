db0689616d69c3d33606478ac70fdb9e855c8cf3
Halid <imhalid@icloud.com>
2024-04-22 18:01:11 +0300

    Refactor GitHub component and add PR table

--- components/github.tsx
+++ components/github.tsx
@@ -1,53 +1,61 @@
 const { GH_TOKEN } = process.env;
 import { graphql } from "@octokit/graphql";
+import PrTable from "./pr-table";
 
-interface Commit {
-  commit: {
-    statusCheckRollup: null | string;
-    url: string;
-    message: string;
-    author: {
-      avatarUrl: string;
-    };
-  };
+export interface Welcome {
+  pullRequests?: PullRequests;
+  issues?: Issues;
 }
 
-interface PullRequest {
-  title: string;
-  url: string;
-  closed: boolean;
-  createdAt: string;
-  closedAt: string;
-  commits: {
-    nodes: Commit[];
-  };
+export interface Issues {
+  nodes?: IssuesNode[];
 }
 
-interface Issue {
-  title: string;
-  closed: boolean;
-  url: string;
-  author: {
-    avatarUrl: string;
-  };
-  repository: {
-    name: string;
-    homepageUrl: string;
-  };
+export interface IssuesNode {
+  title?: string;
+  closed?: boolean;
+  url?: string;
+  author?: Author;
+  repository?: Repository;
 }
 
-interface UserDataProps {
-  data: {
-    user: {
-      pullRequests: {
-        nodes: PullRequest[];
-      };
-      issues: {
-        nodes: Issue[];
-      };
-    };
-  };
+export interface Author {
+  avatarUrl?: string;
 }
+
+export interface Repository {
+  name?: string;
+  homepageUrl?: string;
+}
+
+export interface PullRequests {
+  nodes?: PullRequestsNode[];
+}
+
+export interface PullRequestsNode {
+  title?: string;
+  url?: string;
+  closed?: boolean;
+  createdAt?: Date;
+  closedAt?: Date;
+  commits?: Commits;
+}
+
+export interface Commits {
+  nodes?: CommitsNode[];
+}
+
+export interface CommitsNode {
+  commit?: Commit;
+}
+
+export interface Commit {
+  statusCheckRollup?: null;
+  url?: string;
+  message?: string;
+  author?: Author;
+}
+
 
 export default async function GetRepo() {
 
@@ -56,16 +64,16 @@ export default async function GetRepo() {
       authorization: `token ${GH_TOKEN}`,
     },
   });
-  const { data } = await graphqlWithAuth(`{
+  const { user }: { user: Welcome } = await graphqlWithAuth(`{
   user(login: "imhalid") {
-    pullRequests(last: 100, orderBy: {field: CREATED_AT, direction: DESC}) {
+    pullRequests(last: 1, orderBy: {field: CREATED_AT, direction: DESC}) {
       nodes{
         title
         url
         closed
         createdAt
         closedAt
-        commits(first: 50) {
+        commits(first: 1) {
           nodes {
             commit {
               statusCheckRollup{
@@ -81,7 +89,7 @@ export default async function GetRepo() {
         }
       }
     }
-    issues(last: 100, orderBy: {field: CREATED_AT, direction: DESC}){
+    issues(last: 1, orderBy: {field: CREATED_AT, direction: DESC}){
       nodes {
         title
         closed
@@ -97,58 +105,11 @@ export default async function GetRepo() {
       }
     }
   }
-  `) as UserDataProps
-
-
+  `)
   return (
     <div>
       <h2>Pull Requests</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>URL</th>
-            <th>Status</th>
-            <th>Created At</th>
-            <th>Closed At</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.pullRequests.nodes.map((pr: PullRequest) => (
-            <tr key={pr.url}>
-              <td>{pr.title}</td>
-              <td>{pr.url}</td>
-              <td>{pr.closed ? 'Closed' : 'Open'}</td>
-              <td>{pr.createdAt}</td>
-              <td>{pr.closedAt}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <h2>Issues</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>URL</th>
-            <th>Status</th>
-            <th>Repository</th>
-            <th>Homepage URL</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.issues.nodes.map((issue: Issue) => (
-            <tr key={issue.url}>
-              <td>{issue.title}</td>
-              <td>{issue.url}</td>
-              <td>{issue.closed ? 'Closed' : 'Open'}</td>
-              <td>{issue.repository.name}</td>
-              <td>{issue.repository.homepageUrl}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <PrTable user={user.pullRequests ?? {}} />
     </div>
   );
 }
